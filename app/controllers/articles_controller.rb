ec58ae73a1fd879b7880d1969f5731a62c9e18fa
Konrad Pazgan <hello@konradpazgan.pl>
2021-10-16 17:24:57 +0200

    Create article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,16 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    render plain: params[:article]
   end
 end
