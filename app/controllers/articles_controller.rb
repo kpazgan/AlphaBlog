ea066d6b0e3bddab7d04b65df500c6b5e6cd2a84
Konrad Pazgan <hello@konradpazgan.pl>
2021-10-16 17:10:42 +0200

    Create article index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+# articles controller
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def show
     @article = Article.find(params[:id])
   end
