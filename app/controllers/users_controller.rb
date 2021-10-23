c626166124b804337b93bd6a53b48828fa12aafc
Konrad Pazgan <hello@konradpazgan.pl>
2021-10-23 09:49:07 +0200

    some layout cleanups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha Blog #{@user.name}, you have successfully signed up"
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'new'
     end
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
